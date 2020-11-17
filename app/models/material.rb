ea4c733068d79be8c2b9b898d3da44d119b442a0
Frantz Hedrich <frantzhed@gmail.com>
2020-11-17 11:57:08 +0100

    nothing

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,6 +1,5 @@
 class Material < ApplicationRecord
-  validates :category, presence: true, inclusion: { in: ["ski", "snowboard", "luge"] }
-
   belongs_to :user
   has_many :bookings
+  validates :category, presence: true, inclusion: { in: ["ski", "snowboard", "luge"] }
 end
