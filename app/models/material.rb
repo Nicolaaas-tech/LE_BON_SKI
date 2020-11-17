ff4e04d4239abcd4aa5d4ada3a2b8a7c81ee1cac
Nicolas le Bechec de Redon <nicolaconilebechec@gmail.com>
2020-11-17 11:53:45 +0100

    validations

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,6 +1,9 @@
 class Material < ApplicationRecord
-  validates :category, presence: true, inclusion: { in: ["ski", "snowboard", "luge"] }
+  validates :category, presence: true, inclusion: { in: ["ski", "snowboard", "luge", "racket", "mono-ski"] }
+  validates :size, presence: true, inclusion: { in: ["small", "medium", "large"] }
+  validates :description, presence: true
+  validates :localisation, presence: true
 
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_155618) do
+ActiveRecord::Schema.define(version: 2020_11_17_104136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_155618) do
   create_table "bookings", force: :cascade do |t|
     t.date "start_date"
     t.date "end_date"
-    t.boolean "owner_validation"
+    t.boolean "owner_validation", default: true
     t.bigint "user_id", null: false
     t.bigint "material_id", null: false
     t.datetime "created_at", precision: 6, null: false
