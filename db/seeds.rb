faeda2283632b3a99f3487530acca051f2c8f3a3
Frantz Hedrich <frantzhed@gmail.com>
2020-11-17 13:08:19 +0100

    Added random to faker seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,12 @@ require 'faker'
   Booking.destroy_all
   Material.destroy_all
   User.destroy_all
-
+  category = ["ski", "snowboard", "luge", "racket", "mono-ski"]
+  size = ["small", "medium", "large"]
+  review = ["Great", "Bad", "Very bad", "The best!"]
   5.times do 
     User.create!(email: Faker::Internet.email, password: "12343434")
-    Material.create!(category: "ski", size: "small", description: "Very nice", localisation: "Valtho",user_id: User.first.id)
+    Material.create!(category: category.sample, size: size.sample, description: "Very nice", localisation: Faker::Address.city ,user_id: User.first.id)
     Booking.create!(start_date: "2020-12-12", end_date: "2020-12-16", user_id: User.first.id, material_id: Material.last.id)
-    Review.create!(content: "Bad times", rate: 5, booking_id: Booking.last.id)
+    Review.create!(content: review.sample, rate: rand(0..5), booking_id: Booking.last.id)
   end
