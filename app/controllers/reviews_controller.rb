815cb14c5e6857cbcd308ac7bfa05cbfd00efb65
Nicolas le Bechec de Redon <nicolaconilebechec@gmail.com>
2020-11-18 12:11:32 +0100

    nouveaux fichiers

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,16 @@
 class ReviewsController < ApplicationController
+  before_action :set_booking, only: [:new, :create]
+
+  def new
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
 end
