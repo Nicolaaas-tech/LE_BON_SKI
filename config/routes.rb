f6d949a52c8d4583e7a9addaf4d0f196d3185709
Nicolas le Bechec de Redon <nicolaconilebechec@gmail.com>
2020-11-19 18:02:57 +0100

    dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :materials do
-    resources :bookings, only: [ :new, :create]
+    resources :bookings, only: [ :show, :new, :create]
   end
   resources :bookings, only: [ :index, :show, :new, :create, :edit, :update, :destroy] do
     resources :reviews, only: [ :new, :create, :destroy]
