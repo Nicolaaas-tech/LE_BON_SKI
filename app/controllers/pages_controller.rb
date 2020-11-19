2c10f60d401002a77edded47b84e934e32235341
Nicolas le Bechec de Redon <nicolaconilebechec@gmail.com>
2020-11-19 15:23:12 +0100

    presque finis les controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+
   def home
+  end
+
+  def dashboard
+    @bookings = current_user.bookings
+    @bookingsonmymaterial = current_user.bookingsonmymaterial
   end
 end
